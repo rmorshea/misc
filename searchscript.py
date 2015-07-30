f29c4bfa0e75e0f98c1a36cc93c097a03776801a
Ryan Morshead <ryan.morshead@gmail.com>
2015-07-30 12:37:57 -0700

    ignore and bug fix

--- searchscript.py
+++ searchscript.py
@@ -1,89 +1,9 @@
+from __future__ import absolute_import
+
 import re
 import os
 import subprocess
-
-class MutableNum(object):
-	__val__ = None
-	def __init__(self, v): self.__val__ = v
-	# Comparison Methods
-	def __eq__(self, x):        return self.__val__ == x
-	def __ne__(self, x):        return self.__val__ != x
-	def __lt__(self, x):        return self.__val__ <  x
-	def __gt__(self, x):        return self.__val__ >  x
-	def __le__(self, x):        return self.__val__ <= x
-	def __ge__(self, x):        return self.__val__ >= x
-	def __cmp__(self, x):       return 0 if self.__val__ == x else 1 if self.__val__ > 0 else -1
-	# Unary Ops
-	def __pos__(self):          return self.__class__(+self.__val__)
-	def __neg__(self):          return self.__class__(-self.__val__)
-	def __abs__(self):          return self.__class__(abs(self.__val__))
-	# Bitwise Unary Ops
-	def __invert__(self):       return self.__class__(~self.__val__)
-	# Arithmetic Binary Ops
-	def __add__(self, x):       return self.__class__(self.__val__ + x)
-	def __sub__(self, x):       return self.__class__(self.__val__ - x)
-	def __mul__(self, x):       return self.__class__(self.__val__ * x)
-	def __div__(self, x):       return self.__class__(self.__val__ / x)
-	def __mod__(self, x):       return self.__class__(self.__val__ % x)
-	def __pow__(self, x):       return self.__class__(self.__val__ ** x)
-	def __floordiv__(self, x):  return self.__class__(self.__val__ // x)
-	def __divmod__(self, x):    return self.__class__(divmod(self.__val__, x))
-	def __truediv__(self, x):   return self.__class__(self.__val__.__truediv__(x))
-	# Reflected Arithmetic Binary Ops
-	def __radd__(self, x):      return self.__class__(x + self.__val__)
-	def __rsub__(self, x):      return self.__class__(x - self.__val__)
-	def __rmul__(self, x):      return self.__class__(x * self.__val__)
-	def __rdiv__(self, x):      return self.__class__(x / self.__val__)
-	def __rmod__(self, x):      return self.__class__(x % self.__val__)
-	def __rpow__(self, x):      return self.__class__(x ** self.__val__)
-	def __rfloordiv__(self, x): return self.__class__(x // self.__val__)
-	def __rdivmod__(self, x):   return self.__class__(divmod(x, self.__val__))
-	def __rtruediv__(self, x):  return self.__class__(x.__truediv__(self.__val__))
-	# Bitwise Binary Ops
-	def __and__(self, x):       return self.__class__(self.__val__ & x)
-	def __or__(self, x):        return self.__class__(self.__val__ | x)
-	def __xor__(self, x):       return self.__class__(self.__val__ ^ x)
-	def __lshift__(self, x):    return self.__class__(self.__val__ << x)
-	def __rshift__(self, x):    return self.__class__(self.__val__ >> x)
-	# Reflected Bitwise Binary Ops
-	def __rand__(self, x):      return self.__class__(x & self.__val__)
-	def __ror__(self, x):       return self.__class__(x | self.__val__)
-	def __rxor__(self, x):      return self.__class__(x ^ self.__val__)
-	def __rlshift__(self, x):   return self.__class__(x << self.__val__)
-	def __rrshift__(self, x):   return self.__class__(x >> self.__val__)
-	# Compound Assignment
-	def __iadd__(self, x):      self.__val__ += x; return self
-	def __isub__(self, x):      self.__val__ -= x; return self
-	def __imul__(self, x):      self.__val__ *= x; return self
-	def __idiv__(self, x):      self.__val__ /= x; return self
-	def __imod__(self, x):      self.__val__ %= x; return self
-	def __ipow__(self, x):      self.__val__ **= x; return self
-	# Casts
-	def __nonzero__(self):      return self.__val__ != 0
-	def __int__(self):          return self.__val__.__int__()               # XXX
-	def __float__(self):        return self.__val__.__float__()             # XXX
-	def __long__(self):         return self.__val__.__long__()              # XXX
-	# Conversions
-	def __oct__(self):          return self.__val__.__oct__()               # XXX
-	def __hex__(self):          return self.__val__.__hex__()               # XXX
-	def __str__(self):          return self.__val__.__str__()               # XXX
-	# Random Ops
-	def __index__(self):        return self.__val__.__index__()             # XXX
-	def __trunc__(self):        return self.__val__.__trunc__()             # XXX
-	def __coerce__(self, x):    return self.__val__.__coerce__(x)
-	# Represenation
-	def __repr__(self):         return "%s(%d)" % (self.__class__.__name__, self.__val__)
-	# Define innertype, a function that returns the type of the inner value self.__val__
-	def innertype(self):        return type(self.__val__)
-	# Define set, a function that you can use to set the value of the instance
-	def set(self, x):
-	    if   isinstance(x, (int, long, float)): self.__val__ = x
-	    elif isinstance(x, self.__class__): self.__val__ = x.__val__
-	    else: raise TypeError("expected a numeric type")
-	# Pass anything else along to self.__val__
-	def __getattr__(self, attr):
-	    print("getattr: " + attr)
-	    return getattr(self.__val__, attr)
+from numbers import Variable
 
 class Search(object):
 
@@ -180,8 +100,8 @@ class SearchReplace(Search):
 
 		For more info set 'help' to True"""
 		if help:
-			print(self._help)
-		index = 0
+			self.help()
+		index = Variable(0)
 		current_file = None
 		if not resume:
 			self._on_quit_index = 0
@@ -257,11 +177,10 @@ class SearchReplace(Search):
 	def _handle_y(self, index, p, no, sub):
 		self.replacements.append((p,no,sub))
 		if self._index_dict.get(p,None):
-			self._index_dict[p].append(index)
+			self._index_dict[p].append(index.value)
 		else:
-			self._index_dict[p] = [index]
+			self._index_dict[p] = [index.value]
 		index += 1
-		print index
 		print('')
 
 	def _handle_i(self, index, p, no):
@@ -272,7 +191,16 @@ class SearchReplace(Search):
 		index += 1
 		print('')
 
+	def review_replacements(self):
+		for rep in self.replacements:
+			for last in self.last_find:
+				if rep[0]==last[0] and rep[1]==last[1]:
+					print('L'+str(rep[1])+' @ '+rep[0]+'\n'
+						  'OLD '+last[2]+'NEW '+rep[2])
+
 	def perform_replacements(self):
+		if len(self.replacements)==0:
+			raise AttributeError('nothing to replace')
 		rawin = raw_input('execute all marked replacements (yes/no): ')
 		if rawin == 'yes':
 			repl_dict = dict()
@@ -296,8 +224,7 @@ class SearchReplace(Search):
 
 	def undo(self):
 		if len(self.replacements)==0:
-			print('nothing to undo')
-			return
+			raise AttributeError('nothing to undo')
 		rawin = raw_input('confirm revert (yes/no): ')
 		if rawin == 'yes':
 			for p in self._index_dict:
