3a6610d069c3133539391356a512ec3866bffcd5
Ryan Morshead <ryan.morshead@gmail.com>
2016-04-26 22:55:25 -0700

    changes to dictionaries

--- dictionaries.py
+++ dictionaries.py
@@ -1,65 +1,90 @@
-from weakref import ref as weakref
+import warnings
 
-class refdict(dict):
-
-    def __init__(self, *args, **kwargs):
-        self.update(*args, **kwargs)
-
-    def __setitem__(self, key, value):
-        if not isinstance(key, weakref):
-            key = weakref(key)
-        s = super(refdict, self)
-        s.__setitem__(key, value)
-
+class itable(object):
+    
+    def __init__(self, keys=None, values=None):
+        """A dict-like table mapping keys to values based on indexing"""
+        if keys is not None and values is not None:
+            self.update(keys, values)
+        elif keys is None and values is None:
+            self.__keys__ = []
+            self.__values__ = []
+        else:
+            raise TypeError('Must provide a sequence for both keys and values')
+        
     def __getitem__(self, key):
-        if isinstance(key, weakref):
-            return super(refdict, self).__getitem__(key)
-        for ref in [r for r in self]:
-            if ref() is None:
-                del self[ref]
-            elif key is ref():
-                return super(refdict, self).__getitem__(ref)
-        raise KeyError(str(key))
-
-    def get(self, key, default=None):
         try:
-            return self[key]
-        except KeyError:
-            return default
+            i = self.__keys__.index(key)
+        except ValueError:
+            raise KeyError(key)
+        else:
+            return self.__values__[i]
+        
+    def __setitem__(self, key, value):
+        try:
+            i = self.__keys__.index(key)
+        except ValueError:
+            try:
+                hash(key)
+            except:
+                pass
+            else:
+                warnings.warn('got a hashable mapping; use `dict` instead')
+            self.__keys__.append(key)
+            self.__values__.append(value)
+        else:
+            self.__values__[i] = value
+            
+    def __delitem__(self, key):
+        try:
+            i = self.__keys__.index(key)
+        except ValueError:
+            raise KeyError(key)
+        else:
+            del self.__keys__[i]
+            del self.__values__[i]
+    
+    def __iter__(self):
+        return self.__keys__.__iter__()
 
-    def update(self, *args, **kwargs):
-        if args:
-            if len(args) > 1:
-                raise TypeError("update expected at most 1 arguments, "
-                                "got %d" % len(args))
-            other = dict(args[0])
-            for key in other:
-                self[key] = other[key]
-        for key in kwargs:
-            self[key] = kwargs[key]
+    def update(self, keys, values):
+        if len(keys) != len(values):
+            raise ValueError('keys and values must have the same length')
+        try:
+            map(hash, keys)
+        except:
+            pass
+        else:
+            warnings.warn('got a hashable mapping; use `dict` instead')
 
-    def setdefault(self, key, value=None):
-        if key not in self:
-            self[key] = value
-        return self[key]
+        try:
+            # repeated keys are not removed,
+            # however they will be ignored
+            self.__keys__ = list(keys)[::-1]
+            self.__values__ = list(values)[::-1]
+        except:
+            raise ValueError('keys and values must be iterable')
 
-    def _trim(self):
-        for r in [r for r in self]:
-            if r() is None:
-                del self[r]
-
+    def pop(self, key):
+        try:
+            i = self.__keys__.index(key)
+        except ValueError:
+            raise KeyError(key)
+        else:
+            del self.__keys__[i]
+            return self.__values__.pop(i)
+    
     def keys(self):
-        self._trim()
-        return [ref() for ref in self]
-
+        return self.__keys__[:]
+    
     def values(self):
-        self._trim()
-        get = super(refdict, self).__getitem__
-        return [get(ref) for ref in self]
-
+        return self.__values__[:]
+        
     def items(self):
-        self._trim()
-        keys = [ref() for ref in self]
-        get = super(refdict, self).__getitem__
-        values = [get(ref) for ref in self]
-        return zip(keys, values)
+        return zip(self.__keys__, self.__values__)
+
+    def __repr__(self):
+        body = []
+        for k, v in self.items():
+            body.append(repr(k) + ': ' + repr(v))
+        return '{' + ', '.join(body) + '}'
